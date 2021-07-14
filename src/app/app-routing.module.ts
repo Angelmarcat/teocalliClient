261f10f9a7bbf8c213416c3b6bbc5db1fd26a02c
Angelmarcat <gen2013.pom.angelmarcat2506@gmail.com>
2021-07-13 21:20:10 -0500

    routing and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'myProfile', loadChildren: () => import('./screen/my-profile/my-profile.module').then(m => m.MyProfileModule) }, 
   { path: 'home', loadChildren: () => import('./screen/home/home.module').then(m => m.HomeModule) }, 
   { path: 'lodgings', loadChildren: () => import('./screen/lodgings/lodgings.module').then(m => m.LodgingsModule) }, 
@@ -11,7 +12,10 @@ const routes: Routes = [
   { path: 'paymentHistory', loadChildren: () => import('./screen/payment-history/payment-history.module').then(m => m.PaymentHistoryModule) }, 
   { path: 'viewPayments', loadChildren: () => import('./screen/view-payments/view-payments.module').then(m => m.ViewPaymentsModule) }, 
   { path: 'rentPayment', loadChildren: () => import('./screen/rent-payment/rent-payment.module').then(m => m.RentPaymentModule) }, 
-  { path: 'pay', loadChildren: () => import('./screen/pay/pay.module').then(m => m.PayModule) }];
+  { path: 'pay', loadChildren: () => import('./screen/pay/pay.module').then(m => m.PayModule) },
+  { path: 'login', loadChildren: () => import('./screen/login/login.module').then(m => m.LoginModule) },
+  { path: 'validate', loadChildren: () => import('./screen/validate/validate.module').then(m => m.ValidateModule) },
+  { path: 'register', loadChildren: () => import('./screen/register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
